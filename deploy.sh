d323205c69f7cc94744f06b4ff6735e808aac25c
yair <ydorantes@icarusoftware.com>
2025-05-01 16:11:55 -0600

    added deploy pipeline

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ docker rm -f expenses_back || true
 docker build -t expenses_back .
 docker run -d -p 8000:8000 --restart unless-stopped --name expenses_back expenses_back
 
-echo "Deploy done!"
+echo "Deploy done! :)"
