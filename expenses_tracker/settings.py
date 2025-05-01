28e7ce2109461f530beedae72557c52ea8bb0319
yair <ydorantes@icarusoftware.com>
2025-05-01 16:19:15 -0600

    set jazzmin dashboard theme

--- expenses_tracker/settings.py
+++ expenses_tracker/settings.py
@@ -40,6 +40,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
