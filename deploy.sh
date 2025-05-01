54d3b65d0e74f8124212a7d006cc4a298861fe0f
yair <ydorantes@icarusoftware.com>
2025-05-01 16:25:15 -0600

    set jazzmin dashboard theme

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ echo "Deploying expenses backend..."
 cd /home/yair/projects/expenses/expenses
 
 git pull origin back
-docker rm -f expenses_back || true
 docker build -t expenses_back .
+docker rm -f expenses_back || true
 docker run -d -p 8000:8000 --restart unless-stopped --name expenses_back expenses_back
 
 echo "Deploy done! :)"
