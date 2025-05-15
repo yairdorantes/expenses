ceb1574cf5c3324c3ca8c80e702860280b8c3ac1
yair <ydorantes@icarusoftware.com>
2025-05-15 13:00:10 -0600

    add details in admin pannel recurring trans

--- expenses/models.py
+++ expenses/models.py
@@ -72,4 +72,4 @@ class RecurringTransaction(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"  {self.frequency} | {self.next_date} | {self.end_date}"
+        return f"  {self.frequency} | {self.next_date} | {self.end_date} |  {self.details} "
