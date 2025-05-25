65b48d1477e151b2011e94ea06217846bf38cf8d
yair <ydorantes@icarusoftware.com>
2025-05-25 00:15:31 -0600

    green house

--- src/pages/RecurrentTxs.tsx
+++ src/pages/RecurrentTxs.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FloatingIndicator, Tabs } from "@mantine/core";
+import { Button, FloatingIndicator, Tabs } from "@mantine/core";
 const RecurrentTxs = () => {
   const [rootRef, setRootRef] = useState<HTMLDivElement | null>(null);
   const [value, setValue] = useState<string | null>("1");
@@ -11,30 +11,39 @@ const RecurrentTxs = () => {
     setControlsRefs(controlsRefs);
   };
 
+  const sendNotification = () => {
+    if (Notification.permission === "granted") {
+      new Notification("Weather Update 🌤", {
+        body: "Icoming next expense: Amazon prime",
+        icon: "/expenses.png", // optional
+      });
+    }
+  };
   return (
-    <Tabs variant='none' value={value} onChange={setValue}>
-      <Tabs.List ref={setRootRef}>
-        <Tabs.Tab value='1' ref={setControlRef("1")}>
-          First tab
-        </Tabs.Tab>
-        <Tabs.Tab value='2' ref={setControlRef("2")}>
-          Second tab
-        </Tabs.Tab>
-        <Tabs.Tab value='3' ref={setControlRef("3")}>
-          Third tab
-        </Tabs.Tab>
-
-        <FloatingIndicator
-          target={value ? controlsRefs[value] : null}
-          parent={rootRef}
-          // className={classes.indicator}
-        />
-      </Tabs.List>
-
-      <Tabs.Panel value='1'>First tab content</Tabs.Panel>
-      <Tabs.Panel value='2'>Second tab content</Tabs.Panel>
-      <Tabs.Panel value='3'>Third tab content</Tabs.Panel>
-    </Tabs>
+    <div>
+      <Tabs variant='none' value={value} onChange={setValue}>
+        <Tabs.List ref={setRootRef}>
+          <Tabs.Tab value='1' ref={setControlRef("1")}>
+            First tab
+          </Tabs.Tab>
+          <Tabs.Tab value='2' ref={setControlRef("2")}>
+            Second tab
+          </Tabs.Tab>
+          <Tabs.Tab value='3' ref={setControlRef("3")}>
+            Third tab
+          </Tabs.Tab>
+          <FloatingIndicator
+            target={value ? controlsRefs[value] : null}
+            parent={rootRef}
+            // className={classes.indicator}
+          />
+        </Tabs.List>
+        <Tabs.Panel value='1'>First tab content</Tabs.Panel>
+        <Tabs.Panel value='2'>Second tab content</Tabs.Panel>
+        <Tabs.Panel value='3'>Third tab content</Tabs.Panel>
+      </Tabs>
+      <Button onClick={sendNotification}>noti</Button>
+    </div>
   );
 };
 
