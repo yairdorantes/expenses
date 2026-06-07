2f820432d75984f120d1feea6bd52c64e3fe7db7
yair <yairmasterlol@gmail.com>
2026-06-07 00:37:02 -0600

    news

--- expenses/views.py
+++ expenses/views.py
@@ -89,7 +89,7 @@ class Summary(View):
 
 
 def get_summary():
-    target_date = date(2026, 6, 7)
+    target_date = date(2026, 5, 1)
     expenses = Expense.objects.filter(date__gte=target_date)
 
     total = TOTAL_SAVINGS
